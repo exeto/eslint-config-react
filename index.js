dc3779a6da1977a48691278f917d877c5294f6ad
Timofey Dergachev <t@exeto.me>
2018-02-24 22:45:29 +0300

    Format rules file

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-env node */
-
 module.exports = {
   extends: 'plugin:react/recommended',
   rules: {
@@ -8,49 +6,69 @@ module.exports = {
     'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
     'react/jsx-curly-spacing': ['error', 'never', { allowMultiline: true }],
     'react/jsx-indent-props': ['error', 2],
-    'react/jsx-max-props-per-line': ['error', { maximum: 1, when: 'multiline' }],
-    'react/jsx-no-bind': ['error', {
-      ignoreRefs: true,
-      allowArrowFunctions: true,
-      allowBind: false,
-    }],
+    'react/jsx-max-props-per-line': [
+      'error',
+      { maximum: 1, when: 'multiline' },
+    ],
+    'react/jsx-no-bind': [
+      'error',
+      {
+        ignoreRefs: true,
+        allowArrowFunctions: true,
+        allowBind: false,
+      },
+    ],
     'react/jsx-no-duplicate-props': ['error', { ignoreCase: true }],
-    'react/jsx-pascal-case': ['error', {
-      allowAllCaps: true,
-      ignore: [],
-    }],
+    'react/jsx-pascal-case': [
+      'error',
+      {
+        allowAllCaps: true,
+        ignore: [],
+      },
+    ],
     'react/no-danger': 'warn',
     'react/no-did-mount-set-state': 'error',
     'react/no-did-update-set-state': 'error',
     'react/no-will-update-set-state': 'error',
     'react/no-multi-comp': ['error', { ignoreStateless: true }],
     'react/prefer-es6-class': ['error', 'always'],
-    'react/prop-types': ['error', {
-      ignore: [],
-      customValidators: [],
-      skipUndeclared: false,
-    }],
+    'react/prop-types': [
+      'error',
+      {
+        ignore: [],
+        customValidators: [],
+        skipUndeclared: false,
+      },
+    ],
     'react/self-closing-comp': 'error',
-    'react/jsx-wrap-multilines': ['error', {
-      declaration: true,
-      assignment: true,
-      return: true,
-      arrow: true,
-    }],
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        declaration: true,
+        assignment: true,
+        return: true,
+        arrow: true,
+      },
+    ],
     'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
     'react/jsx-equals-spacing': ['error', 'never'],
     'react/jsx-indent': ['error', 2],
-    'react/no-unused-prop-types': ['error', {
-      customValidators: [
-      ],
-      skipShapeProps: true,
-    }],
+    'react/no-unused-prop-types': [
+      'error',
+      {
+        customValidators: [],
+        skipShapeProps: true,
+      },
+    ],
     'react/style-prop-object': 'error',
-    'react/jsx-tag-spacing': ['error', {
-      closingSlash: 'never',
-      beforeSelfClosing: 'always',
-      afterOpening: 'never',
-    }],
+    'react/jsx-tag-spacing': [
+      'error',
+      {
+        closingSlash: 'never',
+        beforeSelfClosing: 'always',
+        afterOpening: 'never',
+      },
+    ],
     'react/no-array-index-key': 'error',
     'react/require-default-props': 'error',
     'react/void-dom-elements-no-children': 'error',
